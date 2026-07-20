e83c6cba7fd24b24802c78804f00f38a30347fb2
Sona Hasanova <hesenovasonaa@gmail.com>
2026-07-21 01:07:36 +0400

    Add mobile menu toggle and smooth scrolling

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
+const icon=document.querySelector(".icon");
+const nav=document.querySelector("nav");
+const header=document.querySelector("header");
+const links=document.querySelectorAll("nav a");
 
-
+links.forEach(link=>{
+        link.addEventListener("click",function(e){
+                e.preventDefault();
+                let element=document.querySelector(this.getAttribute("href"));
+                element.scrollIntoView({
+                        behavior: "smooth"
+                })
+        })
+})
+function first(){
+        nav.classList.toggle("is-open")
+        header.classList.toggle("is-open")
+}
+icon.addEventListener("click",first);
